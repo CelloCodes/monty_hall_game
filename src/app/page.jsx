315b837fcd807ae16e0ea48fc4eeedebd9e3d962
Marcelo Eduardo Marques Ribas <contato.marcelomk@hotmail.com>
2024-05-13 07:30:52 -0300

    create prize and door components

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import Prize from "@/components/Prize";
+import Door from "@/components/Door";
+
+export default function Home() {
+  return (
+      <div>
+          <Prize />
+          <Door />
+      </div>
+  );
+}
---
+++ src/components/Prize.jsx
@@ -0,0 +1,11 @@
+import styles from "../styles/Prize.module.css"
+export default function Prize() {
+    return (
+        <div className={styles.prize}>
+            <div className={styles.lid}></div>
+            <div className={styles.body}></div>
+            <div className={styles.loop1}></div>
+            <div className={styles.loop2}></div>
+        </div>
+    )
+}
