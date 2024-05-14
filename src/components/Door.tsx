c90015ed25f9155f143c9be9b8b25f95a590c7b7
Marcelo Eduardo Marques Ribas <contato.marcelomk@hotmail.com>
2024-05-14 10:44:07 -0300

    advance door integration and behavior

--- src/components/Door.tsx
+++ src/components/Door.tsx
@@ -2,20 +2,33 @@ import styles from '../styles/Door.module.css'
 import DoorModel from '../models/Door'
 
 interface DoorProps {
-    door: DoorModel
+    value: DoorModel
+    onChange: (new_door: DoorModel) => void
 }
 
 export default function Door(props: DoorProps) {
-    const { door } = props
-    const selected = door.selected ? styles.selected : ''
+    const door = props.value
+    const selected = door.selected && !door.open ? styles.selected : ''
+
+    const switch_selection = e => props.onChange(door.switch_selection())
+    const open_door = e => {
+        e.stopPropagation()
+        props.onChange(door.open_door())
+    }
+
+    function render_door () {
+        return (
+            <div className={styles.door}>
+                <div className={styles.number}>{door.number}</div>
+                <div className={styles.handle} onClick={open_door}></div>
+            </div>
+        )
+    }
 
     return (
-        <div className={styles.area}>
+        <div className={styles.area} onClick={switch_selection}>
             <div className={`${styles.doorframe} ${selected}`}>
-                <div className={styles.door}>
-                    <div className={styles.number}>{door.number}</div>
-                    <div className={styles.handle}></div>
-                </div>
+                {door.open ? false : render_door()}
             </div>
             <div className={styles.floor}></div>
         </div>
---
+++ src/functions/doors.ts
@@ -0,0 +1,18 @@
+import DoorModel from "../models/Door";
+
+export function create_doors(amount: number, selected: number): DoorModel[] {
+    return Array.from({ length: amount }, (_, i) => {
+        const number = i + 1
+        const has_prize = selected === number
+        return new DoorModel(number, has_prize)
+    })
+}
+
+export function update_doors(doors: DoorModel[], modified: DoorModel) {
+    return doors.map(door => {
+        if (door.number === modified.number) {
+            return modified
+        }
+        return modified.open ? door : door.remove_selection()
+    })
+}
